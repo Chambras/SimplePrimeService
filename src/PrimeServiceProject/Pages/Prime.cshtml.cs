f5a7520b3a8605c7c1f1bf3b27fe2d5e1fe99dd0
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2019-07-10 23:12:29 -0400

    fixing special case. Fixes AB#68

--- src/PrimeServiceProject/Pages/Prime.cshtml.cs
+++ src/PrimeServiceProject/Pages/Prime.cshtml.cs
@@ -48,7 +48,7 @@ namespace PrimeServiceProject.Pages
         public bool IsPrime(int number)
         {
             if (number <= 1) return false;
-            //if (number == 2) return true;
+            if (number == 2) return true;
             if (number % 2 == 0) return false;
 
             var boundary = (int)Math.Floor(Math.Sqrt(number));
